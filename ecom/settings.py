a8e996118550ab3388339abee07f45cb56a42301
shailu <shaileshsahu744@gmail.com>
2026-01-15 18:49:41 +0530

    new alb file added

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,9 +1,3 @@
-#this is the alb settings -----------
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-USE_X_FORWARDED_HOST = True
- 
-
-
 
 """
 Django settings for ecom project.
@@ -37,6 +31,21 @@ ALLOWED_HOSTS = [
     'trenzo.shop',
     'www.trenzo.shop',
 ]
+
+#this is the alb settings -----------
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+ 
+# Optional security for HTTPS
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Application definition
 
