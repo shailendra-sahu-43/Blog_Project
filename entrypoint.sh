9cf533c4b0b78bc1b3dee961f29891d119ed97fd
shailu <shaileshsahu744@gmail.com>
2026-02-04 16:38:29 +0530

    new file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+cd /app
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec gunicorn Blog_Project.wsgi:application \
+  --chdir /app \
   --bind 0.0.0.0:8000
